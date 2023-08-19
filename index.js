c2a6afbb6d32099da05ef87d2d07b0c6e01cd9e3
nishantkumar <nishant.soft04@gmail.com>
2023-08-19 12:40:42 +0530

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/api/v1/health", (req, res) => {
+  res.json({ status: "ok" });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
